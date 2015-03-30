c15a5a13097bebd3e8564566623838db70c03dfd
Zack Rock <zacharyrock@hotmail.com>
2015-03-30 01:59:33 -0700

    Flares switch to glowsticks.

---
+++ Assets/Glowsticks_Better/GlowStick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowStick : MonoBehaviour {
+
+	public GameObject greenGlowStick;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			Instantiate(greenGlowStick, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
