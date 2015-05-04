0d92f5531dab944d28ff3600a6568979415ce735
Zack Rock <zacharyrock@hotmail.com>
2015-05-04 01:22:58 -0700

    Bubble zone

---
+++ Assets/BubbleZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleZone : MonoBehaviour {
+
+	public GameObject bubbleParticles;
+	public float bubbleDestroyTime = 2f;
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			Transform player = col.transform;
+			GameObject obj = (GameObject)Instantiate(bubbleParticles, player.position, player.rotation);
+			obj.transform.parent = player;
+//			Destroy(obj, bubbleDestroyTime);
+		}
+	}
+}
