97cd6928c582a010043af8c9e8b165bee9b09875
Zack Rock <zacharyrock@hotmail.com>
2015-05-14 02:50:12 -0700

    Broken pipe bubbles prettier and stop on trigger enter.

---
+++ Assets/TurnOffZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOffZone : MonoBehaviour {
+
+	public ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			particle.emissionRate = 0f;
+		}
+	}
+}
